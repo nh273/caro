dc1d5cc3c2799a51e9b8250398511d871ba35349
Phillip Nguyen <nh273@cornell.edu>
2020-05-01 17:52:32 +0200

    get antidiag passing

--- src/helpers/getters.js
+++ src/helpers/getters.js
@@ -47,3 +47,37 @@ export function getDiag(matrix, coor) {
 
   return diag;
 }
+
+export function getAnti(matrix, coor) {
+  /* Given a row - col matrix and a coordinate [n_row, n_col],
+      return the anti-diagonal (left to right, bot to top) containing the coor */
+  const board_len = matrix.length; // square
+  const n_row = coor[0];
+  const n_col = coor[1];
+
+  // Above and on main anti-diag
+  let col_start = 0; // anti-diag always contains first col
+  // row_end contains the topmost row because we will decrement rows
+  let row_end = 0; // always contains first row
+  let row_start = n_col + n_row; // where we start to decrement, so the bottommost row
+  let col_end = row_start; // because of diagonal symmetry
+
+  if (n_row + n_col >= board_len) {
+    // Change these values if below the main anti-diagonal
+    col_end = board_len - 1; // anti-diag always contains last col
+    row_start = board_len - 1; // always contains last row, which we decrement
+    col_start = n_col + n_row - col_end;
+    row_end = col_start; // because of diagonal symmetry
+  }
+
+  let diag = [];
+  let x = row_start,
+    y = col_start;
+  while (x >= row_end && y <= col_end) {
+    diag.push(matrix[x][y]);
+    x--;
+    y++;
+  }
+
+  return diag;
+}
--- src/helpers/getters.test.js
+++ src/helpers/getters.test.js
@@ -1,16 +1,15 @@
-import { getCol, getDiag } from "./getters";
+import { getCol, getDiag, getAnti } from "./getters";
 
 const smallMatrix = [
   ["x", "o", " "],
   ["o", "o", " "],
   [" ", "o", "x"],
 ];
-
-test("Test the col getter", () => {
-  expect(getCol(smallMatrix, [0, 0])).toEqual(["x", "o", " "]);
-  expect(getCol(smallMatrix, [1, 0])).toEqual(["x", "o", " "]);
-  expect(getCol(smallMatrix, [2, 1])).toEqual(["o", "o", "o"]);
-  expect(getCol(smallMatrix, [1, 2])).toEqual([" ", " ", "x"]);
+test("Test the AntiDiag getter", () => {
+  expect(getAnti(smallMatrix, [0, 0])).toEqual(["x"]);
+  expect(getAnti(smallMatrix, [1, 0])).toEqual(["o", "o"]);
+  expect(getAnti(smallMatrix, [2, 1])).toEqual(["o", " "]);
+  expect(getAnti(smallMatrix, [1, 2])).toEqual(["o", " "]);
 });
 
 test("Test the diag getter", () => {
@@ -18,6 +17,13 @@ test("Test the diag getter", () => {
   expect(getDiag(smallMatrix, [1, 0])).toEqual(["o", "o"]);
   expect(getDiag(smallMatrix, [2, 1])).toEqual(["o", "o"]);
   expect(getDiag(smallMatrix, [1, 2])).toEqual(["o", " "]);
+});
+
+test("Test the col getter", () => {
+  expect(getCol(smallMatrix, [0, 0])).toEqual(["x", "o", " "]);
+  expect(getCol(smallMatrix, [1, 0])).toEqual(["x", "o", " "]);
+  expect(getCol(smallMatrix, [2, 1])).toEqual(["o", "o", "o"]);
+  expect(getCol(smallMatrix, [1, 2])).toEqual([" ", " ", "x"]);
 });
 
 const medMatrix = [
@@ -28,11 +34,12 @@ const medMatrix = [
   ["x", " ", " ", "o", "x"],
 ];
 
-test("Test the Col getter on the medium matrix", () => {
-  expect(getCol(medMatrix, [0, 0])).toEqual(["o", " ", "o", " ", "x"]);
-  expect(getCol(medMatrix, [3, 0])).toEqual(["o", " ", "o", " ", "x"]);
-  expect(getCol(medMatrix, [2, 3])).toEqual(["x", " ", "x", " ", "o"]);
-  expect(getCol(medMatrix, [1, 4])).toEqual([" ", " ", "o", " ", "x"]);
+test("Test the AntiDiag getter on the medium matrix", () => {
+  expect(getAnti(medMatrix, [3, 0])).toEqual([" ", "x", " ", "x"]);
+  expect(getAnti(medMatrix, [0, 0])).toEqual(["o"]);
+  expect(getAnti(medMatrix, [2, 2])).toEqual(["x", "o", " ", " ", " "]);
+  expect(getAnti(medMatrix, [2, 3])).toEqual([" ", "x", "x", " "]);
+  expect(getAnti(medMatrix, [4, 2])).toEqual([" ", " ", "o"]);
 });
 
 test("Test the Diag getter on the medium matrix", () => {
@@ -40,4 +47,11 @@ test("Test the Diag getter on the medium matrix", () => {
   expect(getDiag(medMatrix, [0, 0])).toEqual(["o", "o", " ", " ", "x"]);
   expect(getDiag(medMatrix, [2, 3])).toEqual(["o", " ", "x", " "]);
   expect(getDiag(medMatrix, [1, 4])).toEqual(["x", " "]);
+});
+
+test("Test the Col getter on the medium matrix", () => {
+  expect(getCol(medMatrix, [0, 0])).toEqual(["o", " ", "o", " ", "x"]);
+  expect(getCol(medMatrix, [3, 0])).toEqual(["o", " ", "o", " ", "x"]);
+  expect(getCol(medMatrix, [2, 3])).toEqual(["x", " ", "x", " ", "o"]);
+  expect(getCol(medMatrix, [1, 4])).toEqual([" ", " ", "o", " ", "x"]);
 });
