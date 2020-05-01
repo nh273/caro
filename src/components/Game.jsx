e1ff861aca4edc4f81521362f8c42c76f6c19244
Phillip Nguyen <nh273@cornell.edu>
2020-05-01 11:57:58 +0200

    gameboard formatting

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Game.css";
 
 const BOARD_SIZE = 20; // 20 x 20 board
 
@@ -13,10 +14,12 @@ class Game extends React.Component {
 
   generateBlankBoard = () => {
     const board = new Array(BOARD_SIZE);
+    let i = 0;
     for (var n_row = 0; n_row < BOARD_SIZE; n_row++) {
       const row = new Array(BOARD_SIZE);
       for (var n_col = 0; n_col < BOARD_SIZE; n_col++) {
-        row[n_col] = "";
+        row[n_col] = { index: i, value: "" };
+        i++;
       }
       board[n_row] = row;
     }
@@ -27,25 +30,30 @@ class Game extends React.Component {
     const val = this.state.x_is_next ? "X" : "O";
     let oldBoard = this.state.board;
     let newBoard = [...oldBoard];
-    newBoard[n_row][n_col] = val;
+    newBoard[n_row][n_col].value = val;
     this.setState({ board: newBoard, x_is_next: !this.state.x_is_next });
   };
 
   render() {
-    return this.state.board.map((row, n_row) => {
-      return (
-        <div className="row">
-          {row.map((square, n_col) => {
-            return (
-              <Square
-                value={square}
-                onClick={() => this.updateBoard(n_row, n_col)}
-              />
-            );
-          })}
-        </div>
-      );
-    });
+    return (
+      <div className="game-board">
+        {this.state.board.map((row, n_row) => {
+          return (
+            <div key={n_row} className="board-row">
+              {row.map((square, n_col) => {
+                return (
+                  <Square
+                    key={square.index}
+                    value={square.value}
+                    onClick={() => this.updateBoard(n_row, n_col)}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
